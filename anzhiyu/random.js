38081f80af29d6fea71f5e33adc50fb6c781cf7e
xiao <xxxiaozy@163.com>
2024-04-28 17:10:07 +0800

    Site updated: 2024-04-28 17:10:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post:/abbrlink.html"];function toRandomPost(){
+var posts=["posts/d89579c7.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
